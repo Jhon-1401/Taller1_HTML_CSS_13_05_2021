f8a267ef3651a313dfdc29bedec17aec1ac4158a
Jhon-1401 <jhonguevara1401@gmail.com>
2021-06-01 22:53:08 -0500

    01-06-2020

---
+++ dist/informacion-dist.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var data = [{
+  titulo: "1234",
+  article: {
+    titulo: "",
+    header: "Elo  anuncia que la primera misión tripulada a Marte de SpaceX será en 2024 'si hay suerte' o 'seguramente' en 2026",
+    div: "Los corales son una clase de colonia de animales que se relacionan con los hidrozoos, las medusas y las anémonas de mar. Los corales pétreos, un tipo de coral caracterizado por su esqueleto duro, son el lecho del arrecife. Las colonias de corales pétreos están compuestas por cientos de miles de pólipos individuales.",
+    aside: "https://www.youtube.com/embed/e5Z19udUf9A",
+    p: {
+      src: "https://media.cnnchile.com/sites/4/2020/06/Corales-1140x600-800x400.jpg",
+      title: "La primera misión para enviar humanos a Marte podría estar cerca. 'Si hay suerte', será en 2024, según ha afirmado Elon Musk en una entrevista en Berlín con Axel Springer. La fecha coincide con lo que hace años viene prometiendo el CEO de SpaceX, que apuntaba que en un plazo de diez años lograríamos enviar personas al planeta rojo."
+    }
+  },
+  article2: {
+    titulo: "BBC NEWS | MUNDO",
+    header: "El Perseverance llega a Marte: por qué 3 misiones de tres países diferentes llegaron al planeta rojo casi al mismo tiempo",
+    div: "Febrero ha sido un mes decisivo para la exploración de Marte.",
+    p: 'Este jueves el robot explorador Perseverance de la NASA llegó a Marte, tras un viaje de cerca de 480 millones de km que inició en julio de 2020. El Perseverance descendió hasta la superficie marciana sujetado por una "grúa celestial". Para ello sobrevivió a los llamados "siete minutos de terror", el período de ingreso y descenso en la atmósfera marciana en que la temperatura y el riesgo eran máximos.',
+    footer: {
+      src: "https://www.bbc.com/mundo/noticias-55990181",
+      title: "bbc.com/mundo/noticias"
+    }
+  },
+  article3: {
+    titulo: "BBC NEWS | MUNDO",
+    header: "El Perseverance llega a Marte: por qué 3 misiones de tres países diferentes llegaron al planeta rojo casi al mismo tiempo",
+    div: "Febrero ha sido un mes decisivo para la exploración de Marte.",
+    p: 'Este jueves el robot explorador Perseverance de la NASA llegó a Marte, tras un viaje de cerca de 480 millones de km que inició en julio de 2020. El Perseverance descendió hasta la superficie marciana sujetado por una "grúa celestial". Para ello sobrevivió a los llamados "siete minutos de terror", el período de ingreso y descenso en la atmósfera marciana en que la temperatura y el riesgo eran máximos.',
+    footer: {
+      src: "https://www.bbc.com/mundo/noticias-55990181",
+      title: "bbc.com/mundo/noticias"
+    }
+  }
+}];
